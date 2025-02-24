8712dbd0f67ad175d12e87021f92762d45a06a3d
s851218 <sin0981802858@gmail.com>
2025-02-24 09:30:33 +0800

    更改成 repo 名稱

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   // 開發中、產品路徑
-  base: process.env.NODE_ENV === "production" ? "/React-week4/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/React-week7/" : "/",
   plugins: [react()],
 });
